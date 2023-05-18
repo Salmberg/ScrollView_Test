250ea33fb1d822840d438c7f6520f5ef386fcff3
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-05-18 10:32:03 +0200

    Initial Commit

---
+++ ScrollView_Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ScrollView_Test
+//
+//  Created by David Salmberg on 2023-05-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollView_Test/ScrollView_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollView_TestApp.swift
+//  ScrollView_Test
+//
+//  Created by David Salmberg on 2023-05-18.
+//
+
+import SwiftUI
+
+@main
+struct ScrollView_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
